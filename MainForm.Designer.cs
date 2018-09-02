a783ee7666764354b5251b04057f1b603a15dcd8
Kirurobo <kirurobo@gmail.com>
2018-09-02 22:37:38 +0900

    透明化だけでなく、最前面、キー・マウス操作の透過を切り替えられるようにした。

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,10 +30,12 @@
         {
             this.comboBoxWindowClass = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.buttonAeroNoBorder = new System.Windows.Forms.Button();
             this.buttonNoAeroBorder = new System.Windows.Forms.Button();
             this.buttonRefresh = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxTransparent = new System.Windows.Forms.CheckBox();
+            this.checkBoxTopmost = new System.Windows.Forms.CheckBox();
+            this.checkBoxClickThrough = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // comboBoxWindowClass
@@ -42,73 +44,101 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBoxWindowClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxWindowClass.FormattingEnabled = true;
-            this.comboBoxWindowClass.Location = new System.Drawing.Point(91, 10);
+            this.comboBoxWindowClass.Location = new System.Drawing.Point(48, 10);
             this.comboBoxWindowClass.Name = "comboBoxWindowClass";
-            this.comboBoxWindowClass.Size = new System.Drawing.Size(297, 20);
+            this.comboBoxWindowClass.Size = new System.Drawing.Size(205, 20);
             this.comboBoxWindowClass.TabIndex = 0;
+            this.comboBoxWindowClass.SelectedIndexChanged += new System.EventHandler(this.comboBoxWindowClass_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 12);
+            this.label1.Size = new System.Drawing.Size(29, 12);
             this.label1.TabIndex = 1;
-            this.label1.Text = "対象ウィンドウ";
-            // 
-            // buttonAeroNoBorder
-            // 
-            this.buttonAeroNoBorder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonAeroNoBorder.Location = new System.Drawing.Point(12, 48);
-            this.buttonAeroNoBorder.Name = "buttonAeroNoBorder";
-            this.buttonAeroNoBorder.Size = new System.Drawing.Size(457, 40);
-            this.buttonAeroNoBorder.TabIndex = 1;
-            this.buttonAeroNoBorder.Text = "透明化";
-            this.buttonAeroNoBorder.UseVisualStyleBackColor = true;
-            this.buttonAeroNoBorder.Click += new System.EventHandler(this.buttonAeroNoBorder_Click);
+            this.label1.Text = "対象";
             // 
             // buttonNoAeroBorder
             // 
             this.buttonNoAeroBorder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonNoAeroBorder.Location = new System.Drawing.Point(12, 94);
+            this.buttonNoAeroBorder.Location = new System.Drawing.Point(12, 90);
             this.buttonNoAeroBorder.Name = "buttonNoAeroBorder";
-            this.buttonNoAeroBorder.Size = new System.Drawing.Size(457, 40);
+            this.buttonNoAeroBorder.Size = new System.Drawing.Size(291, 40);
             this.buttonNoAeroBorder.TabIndex = 2;
-            this.buttonNoAeroBorder.Text = "不透明化";
+            this.buttonNoAeroBorder.Text = "元の状態に戻す";
             this.buttonNoAeroBorder.UseVisualStyleBackColor = true;
             this.buttonNoAeroBorder.Click += new System.EventHandler(this.buttonNoAeroBorder_Click);
             // 
             // buttonRefresh
             // 
             this.buttonRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonRefresh.Location = new System.Drawing.Point(394, 7);
+            this.buttonRefresh.Location = new System.Drawing.Point(259, 7);
             this.buttonRefresh.Name = "buttonRefresh";
-            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.Size = new System.Drawing.Size(44, 23);
             this.buttonRefresh.TabIndex = 3;
-            this.buttonRefresh.Text = "一覧更新";
+            this.buttonRefresh.Text = "更新";
             this.buttonRefresh.UseVisualStyleBackColor = true;
             this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.Location = new System.Drawing.Point(13, 149);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(456, 61);
+            this.label2.Size = new System.Drawing.Size(290, 41);
             this.label2.TabIndex = 4;
-            this.label2.Text = "注意\r\n・透明化しても見えないだけでウィンドウは存在します。\r\n・不透明化を押しても完全には戻らない場合もあります。\r\n　その場合は対象のアプリケーションを再起動" +
-    "してください。";
+            this.label2.Text = "注意\r\n・完全には戻らない場合もあります。\r\n　その場合は対象のアプリケーションを再起動してください。";
+            // 
+            // checkBoxTransparent
+            // 
+            this.checkBoxTransparent.AutoSize = true;
+            this.checkBoxTransparent.Location = new System.Drawing.Point(15, 48);
+            this.checkBoxTransparent.Name = "checkBoxTransparent";
+            this.checkBoxTransparent.Size = new System.Drawing.Size(60, 16);
+            this.checkBoxTransparent.TabIndex = 5;
+            this.checkBoxTransparent.Text = "透明化";
+            this.checkBoxTransparent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBoxTransparent.UseVisualStyleBackColor = true;
+            this.checkBoxTransparent.Click += new System.EventHandler(this.checkBoxTransparent_Click);
+            // 
+            // checkBoxTopmost
+            // 
+            this.checkBoxTopmost.AutoSize = true;
+            this.checkBoxTopmost.Location = new System.Drawing.Point(91, 48);
+            this.checkBoxTopmost.Name = "checkBoxTopmost";
+            this.checkBoxTopmost.Size = new System.Drawing.Size(60, 16);
+            this.checkBoxTopmost.TabIndex = 5;
+            this.checkBoxTopmost.Text = "最前面";
+            this.checkBoxTopmost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBoxTopmost.UseVisualStyleBackColor = true;
+            this.checkBoxTopmost.Click += new System.EventHandler(this.checkBoxTopmost_Click);
+            // 
+            // checkBoxClickThrough
+            // 
+            this.checkBoxClickThrough.AutoSize = true;
+            this.checkBoxClickThrough.Location = new System.Drawing.Point(177, 48);
+            this.checkBoxClickThrough.Name = "checkBoxClickThrough";
+            this.checkBoxClickThrough.Size = new System.Drawing.Size(72, 16);
+            this.checkBoxClickThrough.TabIndex = 5;
+            this.checkBoxClickThrough.Text = "操作透過";
+            this.checkBoxClickThrough.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBoxClickThrough.UseVisualStyleBackColor = true;
+            this.checkBoxClickThrough.Click += new System.EventHandler(this.checkBoxClickThrough_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(481, 219);
+            this.ClientSize = new System.Drawing.Size(315, 199);
+            this.Controls.Add(this.checkBoxClickThrough);
+            this.Controls.Add(this.checkBoxTopmost);
+            this.Controls.Add(this.checkBoxTransparent);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.buttonRefresh);
             this.Controls.Add(this.buttonNoAeroBorder);
-            this.Controls.Add(this.buttonAeroNoBorder);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBoxWindowClass);
             this.Name = "MainForm";
@@ -123,10 +153,12 @@
 
         private System.Windows.Forms.ComboBox comboBoxWindowClass;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button buttonAeroNoBorder;
         private System.Windows.Forms.Button buttonNoAeroBorder;
         private System.Windows.Forms.Button buttonRefresh;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxTransparent;
+        private System.Windows.Forms.CheckBox checkBoxTopmost;
+        private System.Windows.Forms.CheckBox checkBoxClickThrough;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -7,109 +7,60 @@ using System.Text;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
-using Ruen;
 
 namespace InvisibleByAero
 {
+    struct WindowStyle
+    {
+    }
+
     public partial class MainForm : Form
     {
         private const string DEFAULT_WINDOWNAME = "MikuMikuDance";
         private const string DEFAULT_WINDOWTITLE = "MMD";
-        private const int GWL_STYLE = -16;
-        private const int GWL_EXSTYLE = -20;
-        private const uint SWP_REFRESH = 0x237;
-        private const uint SWP_NOSIZE = 0x1;
-        private const uint SWP_NOMOVE = 0x2;
-        private const uint SWP_NOZORDER = 0x4;
-        private const uint SWP_NOACTIVATE = 0x10;
-        private const uint SWP_FRAMECHANGED = 0x20;
-        private const uint SWP_ASYNCWINDOWPOS = 0x4000;
-        private const uint SWP_SHOWWINDOW = 0x40;
-        private const uint SWP_NOOWNERZORDER = 0x200;
-        private const uint SWP_NOREPOSITION = 0x200;
-        private const uint SWP_NOSENDCHANGING = 0x400;
-        private const int WS_OVERLAPPEDWINDOW = 0x00CF0000;
-        private const int WS_EX_TRANSPARENT = 0x00000020;
-        private const int WS_EX_LAYERED = 0x00080000;
-        private const int WS_EX_TOPMOST = 0x00000008;
-        private const int WS_EX_OVERLAPPEDWINDOW = 0x00000300;
 
-        private delegate int EnumWindowsDelegate(IntPtr hWnd, int lParam);
-
-        [DllImport("user32.dll")]
-        private static extern int EnumWindows(EnumWindowsDelegate lpEnumFunc, int lParam);
-        [DllImport("user32.dll")]
-        private static extern int IsWindowVisible(IntPtr hWnd);
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
-        [DllImport("user32.dll")]
-        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
-        [DllImport("user32.dll")]
-        private static extern IntPtr FindWindow(string lpszClass, string lpszTitle);
-        [DllImport("user32.dll")]
-        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-        [DllImport("user32.dll")]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int value);
-        [DllImport("user32.dll")]
-        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
-
-        private Dictionary<IntPtr, int> originalStyles = new Dictionary<IntPtr, int>();
-        private Dictionary<IntPtr, int> originalExStyles = new Dictionary<IntPtr, int>();
+        private Dictionary<IntPtr, WindowItem> windowList = new Dictionary<IntPtr, WindowItem>();
 
 
-        private class ProcessItem
+        private class WindowItem
         {
-            public Process Process;
             public IntPtr Handle;
             public string Text;
+            public string ProcessName;
 
-            public ProcessItem(IntPtr handle, Process process, string title)
+            public long OriginalStyles;     // 元のウィンドウスタイル
+            public long OriginalExStyles;   // 元の拡張ウィンドウスタイル
+            public bool Transparent;    // 透過中ならtrue
+            public bool Topmost;        // 最前面中ならtrue
+            public bool Clickthrough;   // キー・マウス操作透過中ならtrue
+
+            public WindowItem(IntPtr hWnd, Process process, string title)
             {
-                this.Process = process;
-                this.Handle = handle;
+                this.ProcessName = process.ProcessName;
+                this.Handle = hWnd;
                 this.Text = title;
+
+                this.Transparent = false;
+                this.Topmost = false;
+                this.Clickthrough = false;
+
+                // ウィンドウ状態を読み込み＆記憶
+                long ws = WinApi.GetWindowLong(hWnd, WinApi.GWL_STYLE);
+                long wsex = WinApi.GetWindowLong(hWnd, WinApi.GWL_EXSTYLE);
+                this.OriginalStyles = ws;
+                this.OriginalExStyles = wsex;
             }
 
             override public string ToString()
             {
-                if (string.IsNullOrEmpty(Text)) return Process.ProcessName;
-                else return Process.ProcessName + " - " + Text;
+                if (string.IsNullOrEmpty(Text)) return ProcessName;
+                else return ProcessName + " - " + Text;
             }
         }
 
         public MainForm()
         {
             InitializeComponent();
-        }
-
-        /// <summary>
-        /// ウィンドウクラス一覧を取得
-        /// </summary>
-        /// 参考： http://muumoo.jp/news/2008/03/26/0enumwindows.html
-        private void GetWindowClasses()
-        {
-            comboBoxWindowClass.Items.Clear();
-            
-            EnumWindows(new EnumWindowsDelegate(delegate(IntPtr hWnd, int lParam)
-                {
-                    StringBuilder sb = new StringBuilder(0x1024);
-                    if (IsWindowVisible(hWnd) != 0 && GetWindowText(hWnd, sb, sb.Capacity) != 0)
-                    {
-                        int pid;
-                        GetWindowThreadProcessId(hWnd, out pid);
-                        Process p = Process.GetProcessById(pid);
-
-                        comboBoxWindowClass.Items.Add(new ProcessItem(hWnd, p, sb.ToString()));
-                        if (p.ProcessName == DEFAULT_WINDOWNAME && sb.ToString() == DEFAULT_WINDOWTITLE)
-                        {
-                            comboBoxWindowClass.SelectedIndex = comboBoxWindowClass.Items.Count - 1;
-                        }
-                    }
-                    return 1;
-                }), 0);
-
-            originalExStyles.Clear();
-            originalStyles.Clear();
         }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -120,93 +71,150 @@ namespace InvisibleByAero
         }
 
         /// <summary>
-        /// 透明化ボタンが押されたときの処理
+        /// ウィンドウクラス一覧を取得
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void buttonAeroNoBorder_Click(object sender, EventArgs e)
+        private void GetWindowClasses()
         {
-            //IntPtr hWnd = FindWindow(comboBoxWindowClass.Text, null);
+            comboBoxWindowClass.Items.Clear();
+            
+            WinApi.EnumWindows(new WinApi.EnumWindowsDelegate(delegate(IntPtr hWnd, long lParam)
+                {
+                    if (windowList.ContainsKey(hWnd)) return 1;
 
-            ProcessItem item = (ProcessItem)comboBoxWindowClass.SelectedItem;
-            if (item == null) return;
+                    StringBuilder sb = new StringBuilder(1024);
+                    if (WinApi.IsWindowVisible(hWnd) != 0 && WinApi.GetWindowText(hWnd, sb, sb.Capacity) != 0)
+                    {
+                        WinApi.GetWindowThreadProcessId(hWnd, out long pid);
+                        Process p = Process.GetProcessById((int)pid);
 
-            IntPtr hWnd = item.Handle;
+                        comboBoxWindowClass.Items.Add(new WindowItem(hWnd, p, sb.ToString()));
+                        if (p.ProcessName == DEFAULT_WINDOWNAME && sb.ToString() == DEFAULT_WINDOWTITLE)
+                        {
+                            comboBoxWindowClass.SelectedIndex = comboBoxWindowClass.Items.Count - 1;
+                        }
+                    }
 
-            if (hWnd == null || hWnd == IntPtr.Zero) return;
+                    return 1;
+                }), 0);
 
-            // ウィンドウ状態を読み込み＆記憶
-            int ws = GetWindowLong(hWnd, GWL_STYLE);
-            int wsex = GetWindowLong(hWnd, GWL_EXSTYLE);
-            if (!originalStyles.ContainsKey(hWnd)) originalStyles[hWnd] = ws;
-            if (!originalExStyles.ContainsKey(hWnd)) originalExStyles[hWnd] = wsex;
-
-            // エアロによる透明化範囲を全体に適用
-            DwmApi.DwmExtendIntoClientAll(hWnd);
-
-            // 枠無しウィンドウにする
-            ws &= ~WS_OVERLAPPEDWINDOW;
-            SetWindowLong(hWnd, GWL_STYLE, ws);
-
-            //// 常に最前面にし、操作は受け付けないようにする
-            ////wsex &= ~WS_EX_OVERLAPPEDWINDOW;
-            //wsex = WS_EX_TRANSPARENT;
-            //wsex |= WS_EX_LAYERED;
-            //wsex |= WS_EX_TOPMOST;
-            //SetWindowLong(hWnd, GWL_EXSTYLE, wsex);
-            //SetWindowPos(hWnd, new IntPtr(-1), 0, 0, 0, 0, SWP_REFRESH);
-
-            SetWindowPos(hWnd, IntPtr.Zero, 0, 0, 0, 0, SWP_REFRESH);
         }
 
         /// <summary>
-        /// 不透明化ボタンが押されたときの処理
+        /// 選択されているウィンドウに合わせてボタンの状態を変更
+        /// </summary>
+        private void UpdateUI()
+        {
+            bool isTransparent = false;
+            bool isTopmost = false;
+            bool isClickThrough = false;
+
+            WindowItem item = (WindowItem)comboBoxWindowClass.SelectedItem;
+            if (item != null)
+            {
+                isTransparent = item.Transparent;
+                isTopmost = item.Topmost;
+                isClickThrough = item.Clickthrough;
+            }
+
+            checkBoxTransparent.Checked = isTransparent;
+            checkBoxTopmost.Checked = isTopmost;
+            checkBoxClickThrough.Checked = isClickThrough;
+        }
+
+        /// <summary>
+        /// 元に戻すボタンが押されたときの処理
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void buttonNoAeroBorder_Click(object sender, EventArgs e)
         {
-            //IntPtr hWnd = FindWindow(comboBoxWindowClass.Text, null);
-
-            ProcessItem item = (ProcessItem)comboBoxWindowClass.SelectedItem;
+            WindowItem item = (WindowItem)comboBoxWindowClass.SelectedItem;
             if (item == null) return;
 
+            item.Transparent = false;
+            item.Topmost = false;
+            item.Clickthrough = false;
+
+            UpdateUI();
+            UpdateWindow(item);
+        }
+
+        /// <summary>
+        /// ウィンドウを再描画させる
+        /// </summary>
+        /// <param name="item"></param>
+        private void UpdateWindow(WindowItem item)
+        {
+            bool isTransparent = false;
+            bool isTopmost = false;
+            bool isClickThrough = false;
+
+            long ws;
+            long wsex;
+
             IntPtr hWnd = item.Handle;
-
-            if (hWnd == null || hWnd == IntPtr.Zero) return;
-
-            // DWMによる透明化範囲をなくす
-            DwmApi.MARGINS margins = new DwmApi.MARGINS(0, 0, 0, 0);
-            DwmApi.DwmExtendFrameIntoClientArea(hWnd, margins);
-
-            int ws;
-            int wsex;
-
-            // 通常ウィンドウにする
-            if (originalStyles.ContainsKey(hWnd))
+            if (!WinApi.IsWindow(hWnd))
             {
-                ws = originalStyles[hWnd];
+                comboBoxWindowClass.Items.Remove(item);
+                return; // ウィンドウが存在しない
+            }
+
+            isTransparent = item.Transparent;
+            isTopmost = item.Topmost;
+            isClickThrough = item.Clickthrough;
+
+            ws = item.OriginalStyles;
+            wsex = item.OriginalExStyles;
+
+            // 透明化状態を切り替え
+            if (isTransparent)
+            {
+                // エアロによる透明化範囲を全体に適用
+                DwmApi.DwmExtendIntoClientAll(hWnd);
+
+                // 枠無しウィンドウにする
+                ws &= ~WinApi.WS_OVERLAPPEDWINDOW;
             } else
             {
-                ws = GetWindowLong(hWnd, GWL_STYLE) | WS_OVERLAPPEDWINDOW;
+                // DWMによる透明化範囲をなくす
+                DwmApi.MARGINS margins = new DwmApi.MARGINS(0, 0, 0, 0);
+                DwmApi.DwmExtendFrameIntoClientArea(hWnd, margins);
             }
-            SetWindowLong(hWnd, GWL_STYLE, ws);
 
-            //// 操作の透過と常に最前面を解除
-            //if (originalExStyles.ContainsKey(hWnd))
-            //{
-            //    wsex = originalExStyles[hWnd];
-            //}
-            //else
-            //{
-            //    wsex = GetWindowLong(hWnd, GWL_EXSTYLE);
-            //    wsex &= ~WS_EX_TRANSPARENT;
-            //    wsex &= ~WS_EX_LAYERED;
-            //    wsex &= ~WS_EX_TOPMOST;
-            //}
-            //SetWindowLong(hWnd, GWL_EXSTYLE, wsex);
+            // 操作は受け付けないようにする
+            if (isClickThrough)
+            {
+                wsex |= WinApi.WS_EX_LAYERED;
+                wsex |= WinApi.WS_EX_TRANSPARENT;
+            }
 
-            SetWindowPos(hWnd, IntPtr.Zero, 0, 0, 0, 0, SWP_REFRESH);
+            // ウィンドウスタイルを適用
+            WinApi.SetWindowLong(hWnd, WinApi.GWL_EXSTYLE, wsex);
+            WinApi.SetWindowLong(hWnd, WinApi.GWL_STYLE, ws);
+
+            WinApi.RECT rect;
+            WinApi.GetWindowRect(hWnd, out rect);
+
+            // 再描画と同時に最前面状態を切り替え
+            WinApi.SetWindowPos(
+                hWnd,
+                (isTopmost ? WinApi.HWND_TOPMOST : WinApi.HWND_NOTOPMOST),
+                rect.left, rect.top,
+                (rect.right - rect.left), (rect.bottom - rect.top),
+                WinApi.SWP_ASYNCWINDOWPOS | WinApi.SWP_FRAMECHANGED | WinApi.SWP_NOCOPYBITS | WinApi.SWP_NOACTIVATE
+                );
+        }
+
+        private void ApplyStyle()
+        {
+            WindowItem item = (WindowItem)comboBoxWindowClass.SelectedItem;
+            if (item == null) return;
+
+            item.Transparent = checkBoxTransparent.Checked;
+            item.Topmost = checkBoxTopmost.Checked;
+            item.Clickthrough = checkBoxClickThrough.Checked;
+
+            UpdateWindow(item);
         }
 
         /// <summary>
@@ -217,6 +225,26 @@ namespace InvisibleByAero
         private void buttonRefresh_Click(object sender, EventArgs e)
         {
             GetWindowClasses();
+        }
+
+        private void comboBoxWindowClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void checkBoxTransparent_Click(object sender, EventArgs e)
+        {
+            ApplyStyle();
+        }
+
+        private void checkBoxTopmost_Click(object sender, EventArgs e)
+        {
+            ApplyStyle();
+        }
+
+        private void checkBoxClickThrough_Click(object sender, EventArgs e)
+        {
+            ApplyStyle();
         }
     }
 }
